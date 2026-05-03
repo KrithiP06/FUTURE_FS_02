b03899ac65040342b3140dde2d1a6fb4ae670a2a
Krithi Prakash <19736krithi@gmail.com>
2026-05-03 17:43:48 +0530

    Refactor server to use PORT environment variable
    Updated server to use environment variable for port.

--- server/server.js
+++ server/server.js
@@ -68,6 +68,10 @@ app.delete("/api/leads/:id", (req, res) => {
     });
 });
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
     console.log("Server running on port 5000");
 });
